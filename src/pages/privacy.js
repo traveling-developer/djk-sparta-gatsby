c042ea5dfa26182ef2381adeb82486e0f1bea1f6
Christoph Menzel <git@menzel-it.de>
2021-09-11 15:51:21 +0200

    Implement a privacy page

---
+++ src/pages/privacy.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import PrivacyComponent from '../components/privacy'
+
+const Privacy = () => {
+    return <Layout pageTitle="Datenschutzerklärung">
+        <PrivacyComponent></PrivacyComponent>
+    </Layout>
+}
+
+export default Privacy
